d67ceb6be6f40facce2be165ae4615608af70a49
fabilqis <fadillah.bilqis@gmail.com>
2018-07-23 14:15:17 +0700

    Finished challenge level 0 to 3

---
+++ index.js
@@ -0,0 +1,79 @@
+//level 0
+
+// fetch("https://swapi.co/api/people/")
+// .then(response => {
+//     return response.json();
+// })
+// .then(data => {
+//     console.log(data);
+// });
+
+//
+
+//level 1
+
+// fetch("https://swapi.co/api/people/")
+// .then(response => {
+//     return response.json();
+// })
+// .then(data => {
+//     console.log(data.results);
+// });
+
+//
+
+//level 2
+
+// const starWars = async() => {
+//     let response = await fetch("https://swapi.co/api/people/")
+//     .then(async (response) =>{
+//         response = await response.json()
+//         return response.results
+//     })
+//     .catch(err => {
+//         return err
+//     })
+//     response.map((res,index) =>{
+//         document.getElementById("character-list").innerHTML = document.getElementById("character-list").innerHTML +  
+//         `<div>No.${index+1}<br>Name: ${res.name}<br>Height: ${res.height}<br>Hair color: ${res.hair_color}<hr></div>`
+
+//     })
+// }
+
+// starWars()
+//
+
+// //level 2.1
+// fetch("https://swapi.co/api/people/")
+//     .then(response => {
+//         return response.json()
+//     })
+//     .then(response => {
+//         response.results.map((res, angka) => {
+//             document.getElementById("character-list").innerHTML = document.getElementById("character-list").innerHTML +
+//                 `<div>No.${angka+1}<br>Name: ${res.name}<br>Height: ${res.height}<br>Hair color: ${res.hair_color}<hr></div>`
+//         })
+//     })
+//
+
+//Level 3
+
+const github = async() => {
+    let response = await fetch("https://api.github.com/users/fabilqis/followers")
+    .then(async (response) =>{
+        response = await response.json()
+        return response
+    })
+    .catch(err => {
+        return err
+    })
+    response.map((res, avatar_url) =>{
+        document.getElementById("githublist").innerHTML = document.getElementById("githublist").innerHTML +  
+        `<div class="d-flex justify-content-center"><div class="container card " style="width: 13rem;">
+        <img src="${res.avatar_url} width="80px" height="140px"></img>
+        <div class="card-body"><h5 class="card-title ">${res.login}</h5><a href="${res.html_url}" class="btn btn-primary"> Go to profile</a></div></div></div>`
+
+    })
+}
+
+github()
