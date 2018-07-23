498f291955fd09711cb5fcc98105766c9ee7a38f
fabilqis <fadillah.bilqis@gmail.com>
2018-07-23 14:51:42 +0700

    add margin

--- index.js
+++ index.js
@@ -69,7 +69,7 @@ const github = async() => {
     })
     response.map((res, avatar_url) =>{
         document.getElementById("githublist").innerHTML = document.getElementById("githublist").innerHTML +  
-        `<div class="d-flex justify-content-center mt-4" align="center"><div class="card " style="width: 15rem;">
+        `<div class="d-flex justify-content-center mb-3 mt-4" align="center"><div class="card " style="width: 15rem;">
         <img src="${res.avatar_url}" width="240px" height="240px"></img>
         <div class="card-body"><h5 class="card-title ">${res.login}</h5><a href="${res.html_url}" class="btn btn-primary" target="_blank"> Go to profile</a></div></div></div>`
 
